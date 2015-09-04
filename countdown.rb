7db665e25fb5800bec2babdaa3c7f240fb3b5f0f
psswf1172 <jennifermabella@gmail.com>
2015-09-04 14:22:58 -0400

    countdown done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
-
-def countdown
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+countdown(10)
+
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
+end
+countdown_with_sleep(5)
